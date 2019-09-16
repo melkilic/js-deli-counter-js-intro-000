7fdefbcf9477dcbf92e3ce9477216a1b690f44ff
Melike KILIC <melike180400@gmail.com>
2019-09-16 16:53:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var currentLine=[];
 function takeANumber(currentLine,newName){
 currentLine.push("${newName}");
-return("Welcome,${newName}. You are number ${currentLine.lenth}in line.");
+return("Welcome,${newName}. You are number ${currentLine.length}in line.");
 }
