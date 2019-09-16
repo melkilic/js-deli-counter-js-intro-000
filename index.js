7f1659523057a44f3a85c6e1b5d3ec240b185412
Melike KILIC <melike180400@gmail.com>
2019-09-16 17:05:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
-// function takeANumber(katzDeliLine,name){
-// katzDeliLine.push("${name}");
-// katzDeliLine.push("${katzDeliLine.length}")
-// return("Welcome,${name}. You are number ${katzDeliLine.length}in line.");
-// }
-// takeANumber(1,"Ada")
-// takeANumber(4,"Grace")
-// takeANumber(6,"Kent")
-function takeANumber(line, name){
-  line.push(name);
-
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
-
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
+function takeANumber(katzDeliLine,name){
+katzDeliLine.push(name);
+katzDeliLine.push(katzDeliLine.length)
+return("Welcome,${name}. You are number ${katzDeliLine.length}in line.");
 }
-takeANumber(katzDeli, "Ada")
-takeANumber(katzDeli, "Grace")
-takeANumber(katzDeli, "Kent")
+takeANumber(1,"Ada")
+takeANumber(4,"Grace")
+takeANumber(6,"Kent")
+// function takeANumber(line, name){
+//   line.push(name);
+// 
+//   console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+// 
+//   return "Welcome, " + name + ". You are number " + line.length + " in line."
+// }
+// takeANumber(katzDeli, "Ada")
+// takeANumber(katzDeli, "Grace")
+// takeANumber(katzDeli, "Kent")
