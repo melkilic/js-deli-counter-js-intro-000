9990039282c1651d54a9eede7451cd860d018517
Melike KILIC <melike180400@gmail.com>
2019-09-16 17:02:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 function takeANumber(katzDeliLine,name){
 katzDeliLine.push("${name}");
+katzDeliLine.push("${katzDeliLine.length}")
 return("Welcome,${name}. You are number ${katzDeliLine.length}in line.");
 }
 takeANumber(1,"Ada")
